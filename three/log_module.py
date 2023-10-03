80dc74f2b51e6a887bb3a936cccb3b26bd6c7cc0
willgbryan <107731540+willgbryan@users.noreply.github.com>
2023-10-03 12:04:12 -0500

    add logging module

---
+++ three/log_module.py
@@ -0,0 +1,11 @@
+import logging
+
+
+def logger():
+
+    logging.basicConfig(
+        level=logging.INFO, 
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
+
+    return logging.getLogger(__name__)
