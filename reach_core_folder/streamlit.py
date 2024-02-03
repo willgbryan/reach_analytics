89d5b2071e3d9cd6349bfb385d18d3c5f0deb682
willgbryan <107731540+willgbryan@users.noreply.github.com>
2024-02-03 16:17:49 -0600

    major ui changes
    support aggregated data display, graph loading, code breakout

--- reach_core_folder/streamlit.py
+++ reach_core_folder/streamlit.py
@@ -1,20 +1,37 @@
 import os
 import marqo
+import pandas as pd
+from openai import OpenAI
 import streamlit as st
 from pipeline import Reach
 from dataset_builder import GPTRequestHandler
-from reusable_utils import extract_code, extract_content_from_gpt_response
+from reusable_utils import (
+    extract_code, 
+    extract_content_from_gpt_response, 
+    get_openai_client,
+    clear_directory,
+)
 
 
-os.environ["OPENAI_API_KEY"] = ""
 dataset_description = None
 
 uploads_dir = 'web_upload/datasets'
+plots_dir = 'web_upload/plots'
+
 if not os.path.exists(uploads_dir):
     os.makedirs(uploads_dir)
 
 with st.sidebar:
+    key = st.sidebar.text_input("Enter your OpenAI Api Key:", type="password")
+    if key:
+        os.environ["OPENAI_API_KEY"] = key
+        client = get_openai_client(api_key=key)
     uploaded_files = st.file_uploader('Choose a folder to upload', accept_multiple_files=True)
+    if os.path.exists(os.path.join(uploads_dir, 'aggregated_data.csv')):
+        df_aggregated = pd.read_csv(os.path.join(uploads_dir, 'aggregated_data.csv'))
+        st.title("Aggregated Data")
+        st.dataframe(df_aggregated)
+    
 
 file_paths = []
 for uploaded_file in uploaded_files:
@@ -25,14 +42,25 @@ for uploaded_file in uploaded_files:
     file_paths.append(os.path.abspath(file_path))
 
 
+ascii_text = """
+`   ____                  __       ___                __      __  _          
+   / __ \___  ____  _____/ /_     /   |  ____  ____ _/ /_  __/ /_(_)_________
+  / /_/ / _ \/ __ `/ ___/ __ \   / /| | / __ \/ __ `/ / / / / __/ / ___/ ___/
+ / _, _/  __/ /_/ / /__/ / / /  / ___ |/ / / / /_/ / / /_/ / /_/ / /__(__  ) 
+/_/ |_|\___/\__,_/\___/_/ /_/  /_/  |_/_/ /_/\__,_/_/\__, /\__/_/\___/____/  
+                                                    /____/    
+"""
+
+st.text(ascii_text)
 prompt = st.chat_input()
 with st.status('Processing Input Params', expanded=True) as status:
 
     if prompt:
+        clear_directory(plots_dir)
         flat_files_exist = any(f.endswith('.csv') for f in os.listdir('web_upload/datasets'))
         if flat_files_exist and not os.path.exists(os.path.join(uploads_dir, 'aggregated_data.csv')):
             st.write("Aggregating supplied data, this may take a few minutes.")
-            handler = GPTRequestHandler()
+            handler = GPTRequestHandler(client)
 
             response, supplied_file_paths, generated_df_summaries = handler.handle_files_and_send_request(
                 file_paths=file_paths,
@@ -47,12 +75,14 @@ with st.status('Processing Input Params', expanded=True) as status:
                 context=[{"role": "user", "content": f"Dataframe Summaries: {generated_df_summaries}"}]
             )
             st.write('Aggregated set created successfully.')
+            set = True
 
         st.write('Beginning analysis...')
         r = Reach(
+                client=client,
                 marqo_client=marqo.Client(url="http://localhost:8882"),
                 marqo_index='validation_testing', 
-                train_set_path='aggregated_data.csv', 
+                train_set_path='web_upload/datasets/aggregated_data.csv', 
                 dataset_description=dataset_description, 
                 goal_prompt=prompt,
                 attempt_validation=True,
@@ -62,5 +92,16 @@ with st.status('Processing Input Params', expanded=True) as status:
         with st.chat_message('user'):
             st.write(f'Result: {so_what}')
             # st.write(f'Analytics code: {validated_code}')
+            if os.path.exists(plots_dir):
+                plot_files = os.listdir(plots_dir)
+                
+                for plot_file in plot_files:
+                    if plot_file.endswith('.png'):
+                        file_path = os.path.join(plots_dir, plot_file)
+                        
+                        st.image(file_path, caption=plot_file, use_column_width=True)
+            else:
+                st.error(f"The directory {plots_dir} does not exist.")
+            st.code(validated_code)
     
     status.update(label='All processes complete...', state='complete', expanded=True)
